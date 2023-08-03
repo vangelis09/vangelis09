08d423c96c2489d8cb00e49953d83b6b387abcf9
Вангелис Мавроматидис <boss.vangelis@mail.ru>
2023-08-03 12:25:20 +0300

    Add files via upload

---
+++ АиП лаба 9 структуры (вар 6 стадион).cpp
@@ -0,0 +1,310 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+bool except = false; //переменная для обработки исключений
+
+struct stadion
+{
+	string name = "empty";      //название стадиона
+	string sport = "empty";     //вид спорта
+	int year = 0;               //год постройки
+	string country = "empty";   //страна
+	string city = "empty";      //город
+	string street = "empty";    //улица
+	int adres = 0;              //адрес
+	int colvo_mest = 0;         //вместимость(чел.)
+	int ploshadki = 0;          //количество площадок
+	string type_of_tab = "empty";//тип табло
+
+	bool flag = 0; //флаговая переменная для поиска нужного элемента
+};
+
+stadion read_from_file(stadion* base, int size)
+//функция чтения из файла
+//принимает указатель на массив структур и размер массива
+//возвращает указатель на заполненный массив структур
+{
+	ifstream fin("input.txt");
+	if (!fin.is_open()) //проверка на открытие файла
+	{
+		cout << "Error opening file!";
+		except = true; //исключение для остановки основной программы
+	}
+	else
+	{
+		while (!fin.eof()) //чтение массива структур из фойла
+		{
+			for (int i = 0; i < size; i++)
+			{
+				fin >> base[i].name;
+				fin >> base[i].sport;
+				fin >> base[i].year;
+				fin >> base[i].country >> base[i].city >> base[i].street >> base[i].adres;
+				fin >> base[i].colvo_mest;
+				fin >> base[i].ploshadki;
+				fin >> base[i].type_of_tab;
+			}
+		}
+	}
+	fin.close(); //закрытие файла
+	return(*base);
+}
+void write_file(stadion* base, int size)
+//функция для записи результатов в файл
+//принимает указатель на массив структур и размер массива
+//тип возвращаемого значения void(нет возвращаемого значения)
+{
+	string path = "output.txt";
+	ofstream fout; //создаем файл с именем "output.txt"
+	fout.open(path);//открываем файл
+	for (int i = 0; i < size; i++)
+	{
+		fout << "Stadion: " << base[i].name << endl;
+		fout << "Sports: " << base[i].sport << endl;
+		fout << "Year: " << base[i].year << endl;
+		fout << "Adress: " << base[i].country << " "
+			<< base[i].city << " " << base[i].street << " " << base[i].adres << endl;
+		fout << "Count of people: " << base[i].colvo_mest << endl;
+		fout << "Count of places: " << base[i].ploshadki << endl;
+		fout << "Type of the scoreboard: " << base[i].type_of_tab << endl;
+		fout << endl;
+	}
+	fout.close();
+}
+
+
+
+
+stadion search(stadion* base, string city, int size)
+//функция для поиска элемента в массиве
+//принимает указатель на массив струтур,
+//элемент, по которому производится поиск нужной структуры
+//и размер массива
+//возвращает массив с расставленными флажками
+{
+	bool flag2 = 0; //переменная для обработки исключения
+	for (int i = 0; i < size; i++) //итерация массива структур
+	{
+		if (base[i].city == city)    //если город указанный в элементе массива
+		{                            //совпадает с переданным в функция значением
+			base[i].flag = 1;        //отмечаем данный элемент флажком
+			flag2 = 1;               //flag2 = 1 сообщает нам, что существует искомый элемент
+		}
+		if (i == size - 1 && flag2 == 0) //если после прохождения массива элемент не найден
+		{
+			cout << "Not found" << endl; //выводится данное сообщение
+		}
+	}
+	return(*base); //возвращаем массив с расставленными флажками
+}
+
+void print(stadion* base, int size)
+//функция для вывода массива на экран
+//принимает указатель на массив структур и размер массива
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << "Stadion: " << base[i].name << endl;
+		cout << "Sports: " << base[i].sport << endl;
+		cout << "Year: " << base[i].year << endl;
+		if (base[i].country == "empty") //если в массиве пуст, 
+		{
+			cout << "Adress: " << base[i].country << endl;  // "empty" в поле "Adress: " выводится только один раз
+															//т.к. адрес разделяется на 4 переменных
+		}                                                    //(страна, город, улица, номер)
+		else
+		{
+			cout << "Adress: " << base[i].country << " "
+				<< base[i].city << " " << base[i].street << " " << base[i].adres << endl;
+		}
+
+		cout << "Count of people: " << base[i].colvo_mest << endl;
+		cout << "Count of places: " << base[i].ploshadki << endl;
+		cout << "Type of the scoreboard: " << base[i].type_of_tab << endl;
+		cout << endl;
+	}
+}
+void menu()
+//функция для общения с пользователем
+{
+	cout << "Choose an action: " << endl;
+	cout << "1. Show list;" << endl;
+	cout << "2. Add information;" << endl;
+	cout << "3. Search;" << endl;
+	cout << "4. Save to the file;" << endl;
+	cout << "5. Order the array;" << endl;
+	cout << "6. Exit." << endl;
+}
+
+stadion* extendedArray(stadion* A, int arraySize, stadion newValue)
+//функция для создания нового массива
+//используется для добавления элемента в массив структур
+//ПРИНИМАЕТ указатель на массив структур,
+//размер массива и новый элемент
+//ВОЗВРАЩАЕТ новый массив структур, размер которого
+//больше размера старого массива на 1
+{
+	stadion* newArray = new stadion[arraySize + 1];
+	for (int i = 0; i < arraySize; ++i)
+	{
+		newArray[i] = A[i];
+	}
+	newArray[arraySize] = newValue;
+	return newArray;
+}
+
+int comp(const stadion* i, const stadion* j)
+{
+	//функция сравнения двух элементов
+	//используется в функции qsort
+	//принимает указатели на элементы массива структур
+	//возвращает разность этих элементов
+
+	int result;
+	result = i->year - j->year;
+	return result;
+
+}
+
+int main()
+{
+	int size; //размер массива
+	cout << "Count of elements: ";
+	cin >> size;
+	stadion* base = new stadion[size]; //создаем основной массив структур
+	stadion* tmp = 0;    //создаем указатель для временного массива
+						//т.к. он будет использоваться при добавлении новых элементов
+
+	read_from_file(base, size); //считываем данные из файла
+	if (except == true)
+	{
+		return 0; //если файл не открылся, то выходим из программы
+	}
+
+	int a = 100; //переменная для работы с меню
+				 //значение 100 - случайное, неравное 6
+
+	do   //программа выполняется до тех пор,
+	{                //пока пользователь не введет 6
+					//для выхода из программы
+		menu();             //вызываем меню
+		cin >> a;         //выбираем один из пунктов меню
+
+		switch (a)
+		{
+		case 1: //вывести на консоль массив структур
+		{
+			system("cls"); //функция очистки консоли
+			print(base, size); //функция вывода массива на консоль
+		}
+		break;
+		case 2: //добавление нового элемента
+		{
+			system("cls");
+			//сперва создаем новый элемент
+			stadion added;
+
+			//вводим с клавиатуры параметры для нового элемента
+			cout << "Info about the new stadion: " << endl;
+			cout << "Name: ";
+			cin >> added.name;
+
+			cout << "Sports: ";
+			cin >> added.sport;
+
+			cout << "Year: ";
+			cin >> added.year;
+
+			cout << "Adress: ";
+			cin >> added.country >> added.city >> added.street >> added.adres;
+
+			cout << "Count of people: ";
+			cin >> added.colvo_mest;
+
+			cout << "Count of places: ";
+			cin >> added.ploshadki;
+
+			cout << "Type of the scoreboard: ";
+			cin >> added.type_of_tab;
+
+			system("cls");
+
+			//создаем временный массив
+			tmp = extendedArray(base, size, added);
+
+			//очищаем старый массив
+			delete[] base;
+
+			//УКАЗАТЕЛЬ СТАРОГО МАССИВА УКАЗЫВАЕТ НА НОВЫЙ
+			base = tmp;
+			// удаляем временный массив
+			tmp = 0;
+			++size; //увеличиваем размер массива на 1
+		}
+		break;
+		case 3: //поиск стадиона по городу, в котором он находится
+		{
+			system("cls");
+			string city;
+			//пользователь вводит нужный город
+			cout << "Where is your stadion: ";
+			cin >> city;
+			cout << endl;
+
+			//функция поиска
+			search(base, city, size);
+			for (int i = 0; i < size; i++) //итерация массива структур
+			{
+				//выводим только те элементы,
+				//для которых функция search() установила flag=1
+				if (base[i].flag == 1)
+				{
+					cout << "Stadion: " << base[i].name << endl;
+					cout << "Sports: " << base[i].sport << endl;
+					cout << "Year: " << base[i].year << endl;
+					cout << "Adress: " << base[i].country << " "
+						<< base[i].city << " " << base[i].street << " " << base[i].adres << endl;
+					cout << "Count of people: " << base[i].colvo_mest << endl;
+					cout << "Count of places: " << base[i].ploshadki << endl;
+					cout << "Type of the scoreboard: " << base[i].type_of_tab << endl;
+					cout << endl;
+				}
+			}
+		}
+		break;
+		case 4:    //сохраняем массив в файл
+		{
+			system("cls");
+			write_file(base, size); //функция записи в файл
+		}
+		break;
+		case 5: //упорядочивание массива структур(сортировка)
+		{
+			system("cls");
+			//сортируем массив по году постройки
+			//от самого старого к самому новому
+			qsort(base, size, sizeof(stadion), (int(*) (const void*, const void*)) comp);
+			//выводим отсортированный массив
+			print(base, size);
+		}
+		break;
+		case 6: return 0; //нажата клавиша 6 для выхода
+
+		default: 
+		{
+			cout << "ERROR!"; //при введении неверного значения выведется ошибка
+			a = 0;
+		}
+		}
+	}while (a != 6);
+
+	delete[]base; //освобождаем выделенную память
+	delete[]tmp;
+
+	return 0;
+}
+
+
+
---
+++ Лаба 8 списки (вар 3) учет автомобилей.cpp
@@ -0,0 +1,334 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <fstream>
+#include <string>;
+
+using namespace std;
+
+int flag = 0;//эта переменная используется только в обработке исключений
+
+struct A
+{
+    string name;      //Фамилия владельцa
+    string marka;     //Марка автомобиля
+    string id;        //Регистрационный номер
+    string year;      //Год выпуска
+};
+struct List
+{
+    A a;        //элемент типа структуры А
+    List* next; //указатель на следующий элемент
+
+};
+
+void First_Init(List** begin) //файловый ввод элемента
+{
+
+    ifstream data("data.txt"); //открываем файл для чтения
+    if (!data.is_open())        //проверка на открытие файла
+    {
+        flag = -1;
+        //флаг возвращает -1, и при вызове функции в главной программе 
+        //выведется сообщение об ошибке открытия файла
+    }
+
+
+    *begin = new List; //выделяем память под новый элемент
+    A a; //создаем этот элемент
+
+    //читаем из файла построчно информацию об элементе
+    getline(data, (*begin)->a.name);
+    getline(data, (*begin)->a.marka);
+    getline(data, (*begin)->a.id);
+    getline(data, (*begin)->a.year);
+
+
+    (*begin)->next = NULL; //указатель на следующий элемент
+
+    List* end = *begin; //указатель на конец списка совпадает с указателем на начало, значит в списке 1 элемент
+
+}
+void Print(List* b) //вывод в консоль
+{
+    List* print = b; //дублируем указатель чтобы случайно не изменить указатель, переданный в функцию
+
+    while (print) //пока print не указывает на NULL
+    {
+        //выводим в консоль строки
+        cout << "Owner's name: " << print->a.name << endl;
+        cout << "Car model: " << print->a.marka << endl;
+        cout << "Registration number: " << print->a.id << endl;
+        cout << "Year of issue: " << print->a.year << endl;
+        cout << "-------------------------" << endl;
+
+        print = print->next; //меняем адрес указателя на следующий
+    }
+
+}
+void File_Print(List* b)
+//функция вывода списка в файл
+{
+    List* print = b;                 //дублируем указатель
+    ofstream list("data_base.txt");  //открываем файл для записи
+
+    if (!list.is_open())             //проверка открытия файла
+    {
+        flag = 5;
+        return;
+    }
+    //----------------------------------------------------------
+    while (print)
+    {
+
+        list << "Owner's name: " << print->a.name << endl;
+        list << "Car model: " << print->a.marka << endl;
+        list << "Registration number: " << print->a.id << endl;
+        list << "Year of issue: " << print->a.year << endl;
+        list << "-------------------------" << endl;
+
+        print = print->next;
+    }
+}
+void Delete(List** begin, string a) //удаление элементов(в видео подробно)
+{
+    if (*begin == NULL) // если указатель на первый элемент равен нулю,
+                        // значит элементов нет, изменяем флаг и выходим из функции
+    {
+        flag = 1;
+        return;
+    }
+    List* t = *begin; //создаем указатель равный указателю на начало списка
+    if (t->a.name == a) //удаляем элемент, имя которого совпадает c введенным из консоли
+                        //если он является первым или единственным элементом в списке
+    {
+        *begin = t->next;
+        delete t;
+        return;
+    }
+    else flag = 2;
+    List* t1 = t->next; //t1 - указатель, который постоянно указывает на элемент,
+                        // идущий после текущего
+
+    while (t1) // С помощью цикла находим нужный элемент и удаляем его
+    {
+        if (t1->a.name == a) //проверяем совпадает ли элемент с нужным
+        {
+            t->next = t1->next;
+            delete t1;
+            return;
+        }
+        else flag = 2;//если не нашли то изменяем флаг
+        t = t1;       //и переходим к следующему
+        t1 = t1->next;
+    }
+}
+void Insert(List** begin, string a, string b, string c, string d)
+//функция вставки элемента в конец списка
+{
+    List* ins = new List; //создаем новый элемент 
+
+    //заполняем элемент значениями, переданными в функцию
+    ins->a.name = a;
+    ins->a.marka = b;
+    ins->a.id = c;
+    ins->a.year = d;
+
+    //создаем указатели типа List
+    List* t = *begin;       //первому присваиваем адрес начала списка
+    List* t1 = t->next;     //второй указывает на элемент, находящийся следующим после t
+
+    //находим конец списка
+    while (t1) //пока указатель на следующий элемент не указывает на NULL
+    {
+        t = t1;          //меняем адрес для указателя на адрес следующего элемента 
+        t1 = t1->next;   //а адрес указателя на следующий элемент на адрес идущего после него
+    }
+
+    //теперь t указывает на конец списка
+    t->next = ins;      //указатель на следующий элемент после конца теперь указывает на ins
+    ins->next = NULL;   //указатель на следующий после ins указывает на NULL, что означает новый конец списка
+}
+void Add_begin(List** begin, string a, string b, string c, string d)
+//функция вставки элемента в начало списка
+{
+    List* t = new List;
+
+
+    t->a.name = a;
+    t->a.marka = b;
+    t->a.id = c;
+    t->a.year = d;
+
+
+
+    t->next = *begin;
+    *begin = t;
+}
+void menu()
+//функция меню, общение с пользователем
+{
+    cout << "Choose an action : \n";
+    cout << "1.Display the database;" << endl;
+    cout << "2.Add an element to the beginning of the list;" << endl;
+    cout << "3.Add an element to the end of the list;" << endl;
+    cout << "4.Delete item;" << endl;
+    cout << "5.Output list to file;" << endl;
+    cout << "6.Exit." << endl;
+}
+void Free(List** begin) //освобождение выделенной памяти
+{
+    if (*begin == NULL) return;
+    List* p = *begin;
+    List* t;
+
+    while (p)
+    {
+        t = p;
+        p = p->next;
+        delete t;
+    }
+    *begin = NULL;
+}
+
+int main()
+{
+    List* begin = NULL;    //создаем указатель на начало списка, присваиваем ему адрес NULL
+    First_Init(&begin);    //вызываем функцию, для прочтения первого элемента из файла
+
+
+    if (flag != 0) //обработка исключения при ошибке открытия файла
+    {
+        cout << "Error opening file!\n\n";
+        return 0;
+    }
+
+    char c; //переменная для работы с оператором выбора switch
+    do //используем цикл do-while, чтобы он выполнился как минимум 1 раз
+    {
+        menu();         //вызываем меню
+        cin >> c;       //пользователь вводит номер выбранной операции
+
+        switch (c) //оператор множественного выбора
+        {
+        case '1': //отображение списка
+        {
+            system("cls"); //очищаем консоль
+            Print(begin);         //вызываем функцию, выводящую список на консоль
+        }
+        break;
+
+        case '2':   //ввод в начало
+        {
+            system("cls");    //очищаем консоль
+            string name1, marka1, id_1, year1;  //переменные для записи параметров: ФИО, марка машины м тд
+
+            //по очереди вводим 
+            cout << "Owner's name: ";
+            cin >> name1;
+
+            cout << endl;
+
+            cout << "Car model: ";
+            cin >> marka1;
+            cout << endl;
+
+            cout << "Registration number: ";
+            cin >> id_1;
+            cout << endl;
+
+            cout << "Year of issue: ";
+            cin >> year1;
+            cout << endl;
+
+            //передаем адрес начала списка и введенные строки в функцию
+            Add_begin(&begin, name1, marka1, id_1, year1); //добавляем элемент в начало списка
+
+        }
+        break;
+
+        case '3':    //ввод в конец
+        {
+            //точно так же как в предыдущем пункте, только
+            //функция вставляет элемент в конец списка
+
+            system("cls");
+            string name1, marka1, id_1, year1;
+
+            cout << "Owner's name: ";
+            cin >> name1;
+
+            cout << endl;
+
+            cout << "Car model: ";
+            cin >> marka1;
+            cout << endl;
+
+            cout << "Registration number: ";
+            cin >> id_1;
+            cout << endl;
+
+            cout << "Year of issue: ";
+            cin >> year1;
+            cout << endl;
+
+            Insert(&begin, name1, marka1, id_1, year1);
+        }
+        break;
+
+        case '4':   //удаление элемента
+        {
+            string name_out; //переменная для вода строки с клавиатуры
+            system("cls");  //очищаем консоль
+
+            //проверяем, не пуст ли список
+            if (begin == NULL) //если пуст, выводим сообщение и завершаем итерацию цикла
+            {
+                cout << "No items to remove!" << endl;
+                break;
+            }
+
+            Print(begin); //выводим весь список на экран
+            cout << endl;
+
+            //просим пользователя ввести имя владельца машины, запись о которой хоти удалить
+            cout << "Enter the name of the owner of the element to be deleted: \n" << endl << endl;
+            cin >> name_out;
+
+            system("cls"); //очищаем консоль
+            flag = 0;             //возвращаем значение 0 переменной для обработки исключений
+                                  //чтобы результат выполнения других функций не повлиял на работу функции удаления
+            Delete(&begin, name_out); //функция удаления, передаем адрес начала списка и введенную строку
+            cout << endl;
+
+            //если в функции поменялось значение переменной обработки исключений
+            // значит элемент не найден
+            if (flag == 2)
+            {
+                cout << "There is no such element!" << endl;  //выводим соответствующее сообщение
+                flag = 0;   //восстанавливаем первоначальное значение пременной flag
+            }
+            else cout << "Deleted successfully!" << endl; //иначе - сообщение об успешном удалении
+
+        }
+        case '5':   //вывод в файл
+        {
+            system("cls"); //очищаем консоль
+
+            File_Print(begin); //вызываем функию записи в файл
+            if (flag == 5) cout << "Error writing to file!" << endl; //если флаг изменился - ошибка записи в файл
+            else cout << "Success!" << endl << endl; //иначе - успешная запись
+
+        }
+        case '6':   //выход
+
+            break;
+
+        default: cout << "Wrong item! \n \n"; //если пользователь ввел номер, которого нет в списке действий
+        }
+    } while (c != '6'); //программа работает до тех пор, пока пользователь не выберет "6.Exit" - выход
+
+
+    Free(&begin); //освобождаем всю выделенную память
+
+    return 0;
+}
